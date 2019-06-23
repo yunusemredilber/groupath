a824cdc62b987501f295f3adb9923df2c64f5f8a
Yunus Emre Dilber <yunusemredilber@yandex.com>
2019-06-23 19:49:16 +0300

    Signup & Profile Pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Welcome!'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/new', to: redirect('/signup')
+  resources :users, except: :index
+  get 'signup', to: 'users#new', as: :signup
 end
