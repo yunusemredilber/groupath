c75adfe478367c5e1ceef90524a3f8bcfd834304
Yunus Emre Dilber <yunusemredilber@yandex.com>
2019-06-27 13:40:24 +0300

    User Operations
    Added tiny futures.

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,14 +1,20 @@
 class FollowController < ApplicationController
+
   def create
     @followed = User.find(params[:id])
     if signed_in? && @followed
-      follow = Follow.new(follower_id: current_user.id, followed_id: @followed.id)
-      if follow.save
-        flash[:success] = 'Followed!'
-        redirect_to profile_path(@followed)
+      if current_user.id == @followed.id
+        flash[:error] = 'Don\'t try to follow yourself!'
+        redirect_to profile_path(current_user)
       else
-        flash[:error] = 'Something wrong!'
-        redirect_to profile_path(@followed)
+        follow = Follow.new(follower_id: current_user.id, followed_id: @followed.id)
+        if follow.save
+          flash[:success] = 'Followed!'
+          redirect_to profile_path(@followed)
+        else
+          flash[:error] = 'Something wrong!'
+          redirect_to profile_path(@followed)
+        end
       end
     else
       flash[:notice] = 'You need to login and follow a valid user!'
