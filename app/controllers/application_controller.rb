6a20517ec45948431c4d754417a543300fe13468
Yunus Emre Dilber <yunusemredilber@yandex.com>
2019-08-11 23:57:54 +0300

    Add custom layout for android app with variants.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :signed_in?, :current_user, :current_users_follows
+  before_action :set_request_variant#, only: :show
 
   # after_action :last_seen
 
@@ -24,6 +25,13 @@ class ApplicationController < ActionController::Base
   # Returns signed in.
   def signed_in?
     current_user
+  end
+
+  # Sets Action Pack variant(s) depending on the browser/device/app or whatever
+  def set_request_variant
+    request.variant = :mobile if request.user_agent =~ /android|Android|blackberry|iphone|ipod|iemobile|mobile|webos/
+    request.variant = :android_app if request.user_agent =~ /AndroidApp/
+    puts "--------------"+request.variant.to_s+"--------------"
   end
 
 #  def last_seen
